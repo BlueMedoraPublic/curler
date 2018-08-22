21055c6b8647f174552150e4d698e1153955e7df
anthonydunckel <tonydunckel@gmail.com>
2018-08-22 12:08:24 -0400

    Added the output of the response boday as well as the header information.

--- src/main/java/bluemedora/Curler.java
+++ src/main/java/bluemedora/Curler.java
@@ -1,6 +1,9 @@
 package bluemedora;
 
+import org.apache.http.HttpResponse;
+import org.apache.http.util.EntityUtils;
 import static org.toilelibre.libe.curl.Curl.curl;
+
 public class Curler {
     public static void main(String[] args) {
         String command = "";
@@ -10,7 +13,13 @@ public class Curler {
         System.out.println("Approximating this curl command:");
         System.out.println("curl " + command);
         System.out.println();
-        System.out.println("Response: ");
-        System.out.println(curl(command));
+        HttpResponse response = curl(command);
+        try {System.out.println("Headers: ");
+            System.out.println(response.toString());
+            System.out.println("Response: ");
+            System.out.println(EntityUtils.toString(response.getEntity(), "UTF-8"));
+        } catch ( Exception e){
+            e.printStackTrace();
+        }
     }
 }
